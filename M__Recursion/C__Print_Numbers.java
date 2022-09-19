7dcc26100938d60b64db456f6d18fce56c41dbef
Dhawa <Dhawaj777@gmail.com>
2022-09-19 14:09:12 +0530

    update

--- M__Recursion/C__Print_Numbers.java
+++ M__Recursion/C__Print_Numbers.java
@@ -1,13 +1,29 @@
 package M__Recursion;
 
 public class C__Print_Numbers {
-    public static void printNumbers(int n){
-        if (n == 5)
+
+    // print 1 to n
+    public static void print(int start, int n){
+        if (start == n){
+            System.out.println(start);
             return;
-        System.out.println(n);
-        printNumbers(n+1);
+        }
+        System.out.print(start + " ");
+        print(start+1, n);
     }
+
+    // print n to 1
+    public static void printReverse(int n){
+        if (n == 1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printReverse(n-1);
+    }
+
     public static void main(String[] args) {
-        printNumbers(1);
+        print(1, 5);
+        printReverse(5);
     }
 }
