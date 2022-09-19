ea8b667eda86ac2d27b8d4ff4b78060c07a2c59c
Dhawa <Dhawaj777@gmail.com>
2022-09-19 14:59:18 +0530

    update

--- M__Recursion/K__last_index.java
+++ M__Recursion/K__last_index.java
@@ -1,24 +1,23 @@
 package M__Recursion;
 
 public class K__last_index {
-    public static int lastIndex(int[] a, int lastIndex, int x){
+    public static int sortedArray(int[] array, int lastIndex, int x){
         if (lastIndex < 0){
             return -1;
         }
 
-        if (a[lastIndex] == x){
+        if (array[lastIndex] == x){
             return lastIndex;
         }
-
-        int tempo = lastIndex(a, lastIndex-1, x);
-        return tempo;
+        return sortedArray(array, lastIndex-1, x);
     }
 
-    public static int output(int[] array){
-        return lastIndex(array, array.length-1, 3);
+    public static int takeInput(int[] array){
+        return sortedArray(array, array.length-1, 3);
     }
+
     public static void main(String[] args) {
-        int[] a = {1,3,5,3,6};
-        System.out.println(output(a));
+        int[] array = {10,8,3,2,5,3};
+        System.out.println(takeInput(array));
     }
 }
