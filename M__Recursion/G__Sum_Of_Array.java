8f65b4b497d36b0667897d375ca734dabde3ad9f
Dhawa <Dhawaj777@gmail.com>
2022-09-19 14:46:00 +0530

    update

--- M__Recursion/G__Sum_Of_Array.java
+++ M__Recursion/G__Sum_Of_Array.java
@@ -2,22 +2,20 @@ package M__Recursion;
 
 public class G__Sum_Of_Array {
 
-    public static int sum(int[] array){
-        if (array.length == 1){
-            return array[0];
+    public static int sortedArray(int[] array, int startIndex){
+        if (startIndex == array.length-1){
+            return array[array.length-1];
         }
-
-        int[] smallArray = new int[array.length-1];
-        for (int i = 1; i < array.length; i++){
-            smallArray[i-1] = array[i];
-        }
-
-        int temp = sum(smallArray);
-        temp = temp + array[0];
-        return temp;
+        int temp = sortedArray(array, startIndex+1);
+        return temp + array[startIndex];
     }
+
+    public static int takeInput(int[] array){
+        return sortedArray(array, 0);
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,7};
-        System.out.print(sum(arr));
+        int[] array = {1,2,3,8,4};
+        System.out.println(takeInput(array));
     }
 }
