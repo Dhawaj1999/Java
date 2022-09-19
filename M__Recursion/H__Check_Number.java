04188f67351d58cb28d6f1ccac245728be5c0051
Dhawa <Dhawaj777@gmail.com>
2022-09-19 14:53:18 +0530

    update

--- M__Recursion/H__Check_Number.java
+++ M__Recursion/H__Check_Number.java
@@ -1,19 +1,19 @@
 package M__Recursion;
 
 public class H__Check_Number {
-    public static int sortedArray(int[] array, int startIndex, int x){
+    public static boolean sortedArray(int[] array, int startIndex, int x){
         if(startIndex == array.length){
-            return -1;
+            return false;
         }
 
         if (array[startIndex] == x){
-            return startIndex;
+            return true;
         }
 
         return sortedArray(array, startIndex+1, x);
     }
 
-    public static int takeInput(int[] array){
+    public static boolean takeInput(int[] array){
         return sortedArray(array, 0, 3);
     }
 
