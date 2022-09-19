03a4ccf7e211ea77ab6c18c2a91c18ce8f198fc1
Dhawa <Dhawaj777@gmail.com>
2022-09-19 13:43:53 +0530

    update

--- M__Recursion/B__X_To_The_Power_N.java
+++ M__Recursion/B__X_To_The_Power_N.java
@@ -2,7 +2,7 @@ package M__Recursion;
 
 public class B__X_To_The_Power_N {
     public static int power(int x, int n) {
-        if (n == 0) {
+        if (n == 1) {
             return x;
         }
         int temp = power(x, n-1);
