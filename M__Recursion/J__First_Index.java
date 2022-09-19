1a7e6c83b66d19c629ff0a2fa878552e70ce99eb
Dhawa <Dhawaj777@gmail.com>
2022-09-19 14:54:50 +0530

    update

--- M__Recursion/J__First_Index.java
+++ M__Recursion/J__First_Index.java
@@ -1,25 +1,24 @@
 package M__Recursion;
 
 public class J__First_Index {
-    public static int firstIndex(int[] a, int startIndex, int x){
-        if (startIndex > a.length-1){
+    public static int sortedArray(int[] array, int startIndex, int x){
+        if(startIndex == array.length){
             return -1;
         }
 
-        if (a[startIndex] == x){
+        if (array[startIndex] == x){
             return startIndex;
         }
 
-        int temp = firstIndex(a, startIndex+1, x);
-        return temp;
+        return sortedArray(array, startIndex+1, x);
     }
 
-    public static int giveAns(int[] a){
-        return firstIndex(a, 0,11);
+    public static int takeInput(int[] array){
+        return sortedArray(array, 0, 8);
     }
 
     public static void main(String[] args) {
-        int[] a = {9,8,10,8};
-        System.out.println(giveAns(a));
+        int[] array = {9,8,10,8};
+        System.out.println(takeInput(array));
     }
 }
