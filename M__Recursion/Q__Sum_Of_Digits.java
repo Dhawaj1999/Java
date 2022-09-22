2ab319fcfe718d11350f7b96cbd6c0eb5c5f8d85
Dhawa <Dhawaj777@gmail.com>
2022-09-22 14:41:55 +0530

    update

--- M__Recursion/Q__Sum_Of_Digits.java
+++ M__Recursion/Q__Sum_Of_Digits.java
@@ -3,17 +3,14 @@ package M__Recursion;
 public class Q__Sum_Of_Digits {
     public static int sumOfDigits(int n){
         if (n == 0){
-            return 0;
+            return n;
         }
 
         int temp = sumOfDigits(n/10);
-        int sum = temp + (n%10);
-        return sum;
-
+        return temp + (n%10);
     }
     public static void main(String[] args) {
         int n = 1234;
-        int r = sumOfDigits(n);
-        System.out.println(r);
+        System.out.println(sumOfDigits(n));
     }
 }
